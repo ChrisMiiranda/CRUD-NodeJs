b4e37182fb19a7dbf10f741a8abbc71e83ae8607
christopher.miranda <christopher.miranda@totvs.com.br>
2021-02-19 16:07:32 -0300

    correção pastas

--- api/src/app.js
+++ api/src/app.js
@@ -11,8 +11,8 @@ const cors = require('cors');
 const app = express();
 
 // ==> Rotas da API:
-const index = require('./index');
-// const usuarioRoute = require('./routes/usuario.routes');
+const index = require('./routes/index');
+// const productRoute = require('./routes/product.routes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,6 +20,6 @@ app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(cors());
 
 app.use(index);
-// app.use('/api/', usuarioRoute);
+// app.use('/api/', productRoute);
 
 module.exports = app;
