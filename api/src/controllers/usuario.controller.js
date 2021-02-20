1ec024c84f4c70e9a6863d305ff4225c2e622de1
christopher.miranda <christopher.miranda@totvs.com.br>
2021-02-20 03:25:29 -0300

    Api Rest finalizada

--- api/src/controllers/usuario.controller.js
+++ api/src/controllers/usuario.controller.js
@@ -17,4 +17,40 @@ exports.createUsuario = async (req, res) => {
       usuario: { usuarioid, nome, endereco, cpf, rg, date_birth }
     },
   });
-};
+}
+
+// ==> Método responsável por listar todos os 'Usuario':
+exports.listAllUsuario = async (req, res) => {
+  const response = await db.query('SELECT * FROM usuario ORDER BY nome ASC');
+  res.status(200).send(response.rows);
+}
+
+// ==> Método responsável por selecionar 'Usuario' pelo 'Id':
+exports.findUsuarioById = async (req, res) => {
+  const usuarioId = parseInt(req.params.id);
+  const response = await db.query('SELECT * FROM usuario WHERE usuarioid = $1', [usuarioId]);
+  res.status(200).send(response.rows);
+}
+
+// ==> Método responsável por atualizar um 'Usuario' pelo 'Id':
+exports.updateUsuarioById = async (req, res) => {
+  const usuarioId = parseInt(req.params.id);
+  const { nome, endereco, stack } = req.body;
+
+  const response = await db.query(
+    "UPDATE usuario SET nome = $1, endereco = $2, stack = $3 WHERE usuarioId = $4",
+    [nome, endereco, stack, usuarioId]
+  );
+
+  res.status(200).send({ message: "Usuario alterado com sucesso!" });
+}
+
+// ==> Método responsável por excluir um 'Usuario' pelo 'Id':
+exports.deleteUsuarioById = async (req, res) => {
+  const usuarioId = parseInt(req.params.id);
+  await db.query('DELETE FROM usuario WHERE usuarioId = $1', [
+    usuarioId
+  ]);
+
+  res.status(200).send({ message: 'Usuario deletado com sucesso!', usuarioId });
+}
--- api/src/routes/usuario.routes.js
+++ api/src/routes/usuario.routes.js
@@ -14,4 +14,16 @@ const usuarioController = require('../controllers/usuario.controller');
 // ==> Rota responsável por criar um novo 'Usuario': (POST): localhost:3000/api/usuarios
 router.post('/usuarios', usuarioController.createUsuario);
 
+// ==> Rota responsável por listar todos os 'Usuario': (GET): localhost:3000/api/usuarios
+router.get('/usuarios', usuarioController.listAllUsuario);
+
+// ==> Rota responsável por selecionar 'Usuario' pelo 'Id': (GET): localhost:3000/api/usuarios/:id
+router.get('/usuarios/:id', usuarioController.findUsuarioById);
+
+// ==> Rota responsável por atualizar 'Usuario' pelo 'Id': (PUT): localhost: 3000/api/usuarios/:id
+router.put('/usuarios/:id', usuarioController.updateUsuarioById);
+
+// ==> Rota responsável por excluir 'Usuario' pelo 'Id': (DELETE): localhost:3000/api/usuarios/:id
+router.delete('/usuarios/:id', usuarioController.deleteUsuarioById);
+
 module.exports = router;
