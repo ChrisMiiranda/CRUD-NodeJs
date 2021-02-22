e1c1578f1270e6040d72062e250d5ee52ead2d67
christopher.miranda <christopher.miranda@totvs.com.br>
2021-02-22 16:46:15 -0300

    navbar

--- api/src/controllers/usuario.controller.js
+++ api/src/controllers/usuario.controller.js
@@ -2,12 +2,10 @@ const db = require("../config/database");
 
 exports.createUsuario = async (req, res) => {
   const { usuarioid, nome, endereco, cpf, rg, date_birth } = req.body;
-  console.log(req.body);
   const { rows } = await db.query(
     "INSERT INTO USUARIO (usuarioid, nome, endereco, cpf, rg, date_birth) VALUES ($1, $2, $3, $4, $5, $6)" ,
    [ usuarioid, nome, endereco, cpf, rg, date_birth ]
   );
-  console.log(rows);
 
   res.status(201).send({
     message: "Usuario adicionado com sucesso!",
---
+++ front/crud-usuario/src/components/navbar/NavBar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export class Navbar extends Component {
+
+    render() {
+        return (
+            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+                <div className="collpase navbar-collapse">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="navbar-item">
+                            <Link to="/create" className="nav-link">Listagem</Link>
+                        </li>
+                        <li className="navbar-item">
+                            <Link to="/user" className="nav-link">Cadastro</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
